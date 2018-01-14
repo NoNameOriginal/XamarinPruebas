c91143a348b6b718c65f237590fcb3cb19c127d7
Christian Londono Canas <clondo46@eafit.edu.co>
2018-01-14 00:25:29 -0500

    fasd

---
+++ XamarinPruebas/XamarinPruebas/XamarinPruebas/Models/Rate.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace XamarinPruebas.Models
+{
+    public class Rate
+    {
+        public int RateId { get; set; }
+
+        public string Code { get; set; }
+
+        public int TaxRate { get; set; }
+
+        public string Name { get; set; }
+    }
+}
