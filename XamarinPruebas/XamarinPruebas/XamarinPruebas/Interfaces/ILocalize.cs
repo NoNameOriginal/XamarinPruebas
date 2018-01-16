e870d45bf04b7952b6846ba78555f2626b317cc7
Christian Londono Canas <clondo46@eafit.edu.co>
2018-01-16 01:54:33 -0500

    Multi-Languages

---
+++ XamarinPruebas/XamarinPruebas/XamarinPruebas/Interfaces/ILocalize.cs
@@ -0,0 +1,12 @@
+﻿
+namespace XamarinPruebas.Interfaces
+{
+    using System.Globalization;
+
+    public interface ILocalize
+    {
+        CultureInfo GetCurrentCultureInfo();
+
+        void SetLocale(CultureInfo ci);
+    }
+}
